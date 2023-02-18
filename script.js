ffb876f5302ce82be69e1a6ed931b5d67751a66e
Amit0458 <amitkumar51997@gmail.com>
2023-02-18 19:10:40 +0530

    tech Stacks added

---
+++ script.js
@@ -0,0 +1,8 @@
+let hamBurger = document.querySelector('.hamburger');
+let nav = document.querySelector('#nav-menu');
+let hmAfterClick = document.querySelector('.hamburger_After');
+
+hamBurger.addEventListener('click', () => {
+    hmAfterClick.classList.toggle('HmafterClick');
+    
+})
