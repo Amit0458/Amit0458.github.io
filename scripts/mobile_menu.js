9609f16bd9c3ba4637c2f2d5a86d2f3d8e272e24
Amit0458 <amitkumar51997@gmail.com>
2023-03-23 01:30:08 +0530

    ...

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,7 +1,16 @@
 let hamBurger = document.querySelector('.hamburger');
-let nav = document.querySelector('#nav-menu');
-let hmAfterClick = document.querySelector('.hamburger_After');
+let nav = document.querySelector('.nav_list_ul');
+let cross = document.querySelector('.cross');
 
 hamBurger.addEventListener('click', () => {
-    hmAfterClick.classList.toggle('HmafterClick');
+    nav.classList.toggle('nav_visibility');
+    cross.classList.toggle('cross_visibility');
+    hamBurger.classList.toggle('hamburger_visisibility');
+    console.log("here");
+})
+cross.addEventListener('click', () => {
+    hamBurger.classList.toggle('hamburger_visisibility');
+    nav.classList.toggle('nav_visibility');
+    cross.classList.toggle('cross_visibility');
+    console.log("here");
 })
