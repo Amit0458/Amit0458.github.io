eb18393aec252d3f4d3ad1ff68c4d5df6cbf0507
Amit0458 <amitkumar51997@gmail.com>
2023-03-23 23:34:18 +0530

    final touch

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,22 +1,24 @@
-let hamBurger = document.querySelector('.hamburger');
-let nav = document.querySelector('.nav_list_ul');
-let cross = document.querySelector('.cross');
+const hamBurger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav_list_ul');
+const cross = document.querySelector('.cross');
 
 hamBurger.addEventListener('click', () => {
     nav.classList.toggle('nav_visibility');
     cross.classList.toggle('cross_visibility');
     hamBurger.classList.toggle('hamburger_visisibility');
-    console.log("here");
+    // console.log("here");
 })
 cross.addEventListener('click', () => {
     hamBurger.classList.toggle('hamburger_visisibility');
     nav.classList.toggle('nav_visibility');
     cross.classList.toggle('cross_visibility');
-    console.log("here");
+    // console.log("here");
 })
-// window.onscroll = () => {
-//     nav.classList.remove('nav_visibility');
-//     // cross.classList.remove('cross_visibility');
-//     cross.classList.toggle('cross_visibility');
-//     // navlist.classList.remove('open')
-// };
+window.onscroll = () => {
+    if(cross.classList.contains("cross_visibility")) {
+        console.log("yes");
+        cross.classList.toggle('cross_visibility');
+        hamBurger.classList.toggle('hamburger_visisibility');
+        nav.classList.toggle('nav_visibility');
+    }
+};
