7b19f93142d1d2ae355b50715a6a21b3f315c276
Amit0458 <amitkumar51997@gmail.com>
2023-05-17 20:10:06 +0530

    new designes added

--- scripts/mobile_menu.js
+++ scripts/mobile_menu.js
@@ -1,24 +1,39 @@
-const hamBurger = document.querySelector('.hamburger');
+//<<<---- Hamburger toggle ---->>>
+const hamBurger = document.querySelector('#hamburger');
+const bars = document.querySelector('.fa-bars');
+const cross = document.querySelector('.fa-x');
 const nav = document.querySelector('.nav_list_ul');
-const cross = document.querySelector('.cross');
 
 hamBurger.addEventListener('click', () => {
-    nav.classList.toggle('nav_visibility');
     cross.classList.toggle('cross_visibility');
-    hamBurger.classList.toggle('hamburger_visisibility');
-    // console.log("here");
-})
-cross.addEventListener('click', () => {
-    hamBurger.classList.toggle('hamburger_visisibility');
-    nav.classList.toggle('nav_visibility');
-    cross.classList.toggle('cross_visibility');
-    // console.log("here");
-})
-window.onscroll = () => {
-    if(cross.classList.contains("cross_visibility")) {
-        console.log("yes");
-        cross.classList.toggle('cross_visibility');
-        hamBurger.classList.toggle('hamburger_visisibility');
-        nav.classList.toggle('nav_visibility');
+    bars.classList.toggle('hamburger_visisibility');
+    if(nav.classList.contains('nav_visibility')) {
+        nav.classList.remove('nav_visibility');
+    }else {
+        nav.classList.add('nav_visibility');
     }
-};
+})
+
+
+//<<<---- Day/Night toggle ---->>>
+const toggle = document.querySelector('#toggle');
+const sun = document.querySelector('.fa-sun');
+const moon = document.querySelector('.fa-moon');
+toggle.addEventListener('click', () => {
+    sun.classList.toggle('sun_visibility');
+    moon.classList.toggle('moon_visibility');
+    if(document.body.classList.contains("light-mode")) {
+        document.body.classList.remove('light-mode');
+    }else {
+        document.body.classList.add('light-mode');
+    }
+})
+
+window.addEventListener("scroll", () => {
+    const position = window.scrollY;
+    // console.log(position);
+    if(position > 20) {
+
+    }
+});
+
